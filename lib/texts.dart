723ce5a9a1806fa08635718005878d2bee1cb537
Isabella <izzyplays07@gmail.com>
2026-02-17 20:28:38 +0000

    a

--- lib/texts.dart
+++ lib/texts.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-Widget header(String text) => Text(text, style: TextStyle(fontSize: 24));
+class DawnText {
+  static Widget header(String text) =>
+      Text(text, style: TextStyle(fontSize: 24));
+}
