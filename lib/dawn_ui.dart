7ba90e36e73dd887619fce2a8a6144813b1727e3
Isabella <izzyplays07@gmail.com>
2026-02-17 20:27:54 +0000

    add texts

--- lib/dawn_ui.dart
+++ lib/dawn_ui.dart
@@ -4,5 +4,6 @@ export 'future_builder.dart';
 export 'popup_menu.dart';
 export 'util.dart';
 export 'wrapped_text.dart';
+export "texts.dart";
 
 export "prompts/prompts.dart";
---
+++ lib/texts.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Widget header(String text) => Text(text, style: TextStyle(fontSize: 24));
